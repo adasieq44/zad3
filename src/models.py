bd4e6ef9f0ea17b07b06db3189d02e4038d2bab6
Julia <julia.kurowska07@gmail.com>
2026-03-16 10:53:51 +0100

    Porzadkowanie plikow

---
+++ src/models.py
@@ -0,0 +1,93 @@
+class Parameters(BaseModel):
+    apartments_json_path: str = 'data/apartments.json'
+    tenants_json_path: str = 'data/tenants.json'
+    transfers_json_path: str = 'data/transfers.json'
+    bills_json_path: str = 'data/bills.json'
+
+
+class Room(BaseModel):
+    name: str
+    area_m2: float
+
+
+class Apartment(BaseModel):
+    key: str
+    name: str
+    location: str
+    area_m2: float
+    rooms: Dict[str, Room]
+
+    @staticmethod
+    def from_json_file(file_path: str) -> Dict[str,'Apartment']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, dict), "Expected a dictionary of apartments"
+        return {key: Apartment(**apartment) for key, apartment in data.items()}
+
+    
+class Tenant(BaseModel):
+    name: str
+    apartment: str
+    room: str
+    rent_pln: float
+    deposit_pln: float
+    date_agreement_from: str
+    date_agreement_to: str
+
+    @staticmethod
+    def from_json_file(file_path: str) -> Dict[str,'Tenant']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, dict), "Expected a dictionary of tenants"
+        return {key: Tenant(**tenant) for key, tenant in data.items()}
+
+class ApartamentSettlement(BaseModel):
+    name: str
+    apartment: str
+    room: str
+    rent_pln: float
+    deposit_pln: float
+    date_agreement_from: str
+    date_agreement_to: str
+
+    @staticmethod
+    def from_json_file(file_path: str) -> Dict[str,'ApartamentSettlement']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, dict), "Expected a dictionary of tenants"
+        return {key: ApartamentSettlement(**tenant) for key, tenant in data.items()}
+
+class Transfer(BaseModel):
+    amount_pln: float
+    date: str
+    settlement_year: int | None
+    settlement_month: int | None
+    tenant: str
+
+    @staticmethod
+    def from_json_file(file_path: str) -> List['Transfer']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, list), "Expected a list of transfers"
+        return [Transfer(**transfer) for transfer in data]
+
+
+class Bill(BaseModel):
+    amount_pln: float
+    date_due: str
+    apartment: str
+    settlement_year: int
+    settlement_month: int
+    type: str
+
+    @staticmethod
+    def from_json_file(file_path: str) -> List['Bill']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, list), "Expected a list of bills"
+        return [Bill(**bill) for bill in data]
