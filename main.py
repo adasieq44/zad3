7aa97d58749505b2311ec1198d69fd37602afd57
Mateusz <matikonkursiakpl@gmail.com>
2026-03-16 11:00:14 +0100

    Zmiana pliku main.py

--- main.py
+++ main.py
@@ -135,3 +135,9 @@ if __name__ == '__main__':
         for transfer in manager.transfers:
             if transfer.tenant == tenant.name:
                 print('  ', transfer.amount_pln, transfer.date, transfer.settlement_year, transfer.settlement_month)
+                
+    for tenant in manager.tenants.values():                
+        print(tenant.name, tenant.apartment, tenant.room, tenant.rent_pln, tenant.deposit_pln, tenant.date_agreement_from, tenant.date_agreement_to)
+        for transfer in manager.transfers:
+            if transfer.tenant == tenant.name:
+                print('  ', transfer.amount_pln, transfer.date, transfer.settlement_year, transfer.settlement_month)
